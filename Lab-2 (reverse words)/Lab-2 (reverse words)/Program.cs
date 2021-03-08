a1d3a069ba5d0b19a225fcebcba628bbd9d7850c
ACC-Stas <79222536+ACC-Stas@users.noreply.github.com>
2021-03-09 00:18:54 +0300

    create Lab-2 (reverse words)

---
+++ Lab-2 (reverse words)/Lab-2 (reverse words)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseWords {
+    class Program {
+        static void Main(string[] args) {
+            string words =" " + Console.ReadLine();
+            StringBuilder reverseWords = new StringBuilder(words.Length + 1);
+            for(int wordBegin = words.Length - 1, wordEnd = words.Length - 1; wordBegin >= 0; wordBegin--) {  
+                if(words[wordBegin] == ' ') {
+                    while(words[wordEnd] == ' ') {
+                        if(wordEnd <= 0) {
+                            break;
+                        }
+                        wordEnd--;
+                    }
+                    if(wordBegin < wordEnd) {
+                        reverseWords.Append(words.Substring(wordBegin, wordEnd - wordBegin + 1));
+                        if(wordBegin == 0) {
+                            break;
+                        } else {
+                            wordEnd = wordBegin - 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(reverseWords);
+        }
+    }
+}
