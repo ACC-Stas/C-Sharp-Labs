443d0f4a0a2929dfb3564c73a062527186a70c2c
ACC-Stas <79222536+ACC-Stas@users.noreply.github.com>
2021-03-17 23:18:45 +0300

    Create Vehicle, and basic Car

---
+++ Lab-2 (transport)/Lab-2 (transport)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Transport {
+    class Program {
+        static void Main(string[] args) {
+            Car car = new Car(145, 4);
+            Car car1 = new Car(1234, 6);
+            Console.WriteLine(car.Move());
+            Console.ReadKey();
+            Console.WriteLine(car.Cost);
+            Console.WriteLine(car.Number);
+            Console.WriteLine(car1.Number);
+            Console.WriteLine(car.Stop());
+            Console.ReadKey();
+        }
+    }
+}
