86b30b65e56f0d35c4f3cc4d6b3ea781ee021518
ACC-Stas <79222536+ACC-Stas@users.noreply.github.com>
2021-03-25 18:26:58 +0300

    Add MathLibrary and MathClient

---
+++ MathClient/MathClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MathClient {
+    class Program {
+        [DllImport("../../../../MathLibrary/Debug/MathLibrary.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Sum(int a, int b);
+        static void Main(string[] args) {
+            Console.WriteLine(Sum(5, 6));
+        }
+    }
+}
